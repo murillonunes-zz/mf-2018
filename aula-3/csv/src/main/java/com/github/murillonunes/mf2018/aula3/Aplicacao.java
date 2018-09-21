bd223e647c572fddbf56a2557a78d6569997f4af
murillonunes <murillo.nns@gmail.com>
2018-09-21 04:11:57 -0300

    Primeira versão funcional da Aplicacao

--- aula-3/csv/src/main/java/com/github/murillonunes/mf2018/aula3/Aplicacao.java
+++ aula-3/csv/src/main/java/com/github/murillonunes/mf2018/aula3/Aplicacao.java
@@ -10,6 +10,8 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.*;
+import java.util.Map.Entry;
 
 /**
  * Essa classe tem como objetivo solucionar a questão proposta no exercício 11 da aula 3
@@ -22,6 +24,7 @@ import java.net.URLConnection;
 public class Aplicacao {
 
     public static final String URL_PADRAO = "http://repositorio.dados.gov.br/educacao/CADASTRO%20DAS%20IES_2011.csv";
+    public static final int LINHAS_IGNORADAS = 11;
 
     /**
      * O programa funciona da seguinte maneira:
@@ -48,8 +51,53 @@ public class Aplicacao {
         InputStreamReader csv = new InputStreamReader(((URLConnection) conexao).getInputStream());
         BufferedReader buffer = new BufferedReader(csv);
 
+        int linhaAtual = 0;
+        String linhaLendo;
+        HashMap<String, Integer> quantIESPorEstado = new HashMap<String, Integer>();
+        while ((linhaLendo = buffer.readLine()) != null) {
+            linhaAtual++;
+            if (linhaAtual > LINHAS_IGNORADAS) {
+                String[] dados = linhaLendo.split(";");
+                if (dados != null && dados.length > 9 && !dados[0].isEmpty()) {
+                    quantIESPorEstado.put(dados[9], quantIESPorEstado.getOrDefault(dados[9], 0) + 1);
+                }
+            }
+        }
 
+        Map<String, Integer> ordemDescrescente = ordenar(quantIESPorEstado, false);
+        for (Map.Entry<String, Integer> entry : ordemDescrescente.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println(key + ": " + value);
+        }
 
+    }
+
+    private static Map<String, Integer> ordenar (Map<String, Integer> mapOriginal, final boolean ordem) {
+        List<Entry<String, Integer>> list = new LinkedList<Entry<String, Integer>>(mapOriginal.entrySet());
+
+        Collections.sort(list, new Comparator<Entry<String, Integer>>() {
+            public int compare(Entry<String, Integer> o1,
+                               Entry<String, Integer> o2)
+            {
+                if (ordem)
+                {
+                    return o1.getValue().compareTo(o2.getValue());
+                }
+                else
+                {
+                    return o2.getValue().compareTo(o1.getValue());
+
+                }
+            }
+        });
+
+        Map<String,Integer> mapOrdenado = new LinkedHashMap<String, Integer>();
+        for (Entry<String, Integer> entry : list) {
+            mapOrdenado.put(entry.getKey(), entry.getValue());
+        }
+
+        return mapOrdenado;
     }
 
 }
