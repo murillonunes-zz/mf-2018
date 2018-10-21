4c47089c1b36cf5830f61ed722a19a78be4f68ac
Murillo Nunes <murillo.nns@gmail.com>
2018-10-21 13:02:26 -0300

    cnes: diretório e estrutura inicial

---
+++ aula-5/cnes/src/main/java/com/github/murillonunes/mf2018/cnes/Programa.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018.
+ * Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+
+package com.github.murillonunes.mf2018.cnes;
+
+public final class Programa {
+
+    
+
+}
